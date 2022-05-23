46df5fcced5dfc456c6ad8eccdba570b4bb21e12
Tequin Lake <76464882+Laketequin1@users.noreply.github.com>
2022-05-23 21:56:14 +0000

    c

--- bottombit.php
+++ bottombit.php
@@ -1,10 +1,6 @@
     <footer>
-      <p>&nbsp;</p>
-      <h6>cool cool</h6>
     </footer>
-                
-        
+      
   </div> <!-- / wrapper -->
-    
             
 </body>
--- topbit.php
+++ topbit.php
@@ -4,14 +4,13 @@
       
 <head>
   <meta charset="utf-8">
-  <meta name="description" content="games, apps, ">
+  <meta name="description" content="Strategy games ratings">
   <meta name="author" content="Tequin Lake, Malachi Lang">
-  <meta name="keywords" content="games, apps, ratings, Malachi Lang, Tequin Lake, Laketequin1, AwSnap">
+  <meta name="keywords" content="Strategy, Games, Apps, Ratings, Malachi Lang, Tequin Lake, Laketequin1, AwSnap">
   
   <title>Strategy Games</title>
 
   <!--Styling Files-->
-  <link rel="stylesheet" href="/css/font-awesome.min.css">
   <link rel="stylesheet" href="/css/style.css">    <!-- custom style sheet -->
   <link rel="stylesheet" href="/css/responsive.css">    <!-- custom responsive style sheet -->
 
@@ -25,17 +24,17 @@
 
 </head>
 
-<body>
+<body class="background" >
         
-    <div class="container">
+    <div class="container debug-box">
     
-      <!-- logo / small image goes here -->
+      <!-- logo -->
       <div class="box logo">
-          <a href="index.php"><img src="images/logo.jpg" height="45" alt="Dice" /></a>
-      </div> <!-- / logo -->
+          <a href="index.php"><img src="images/logo.png" class="icon" alt="Home logo" /></a>
+      </div> <!-- / box logo -->
 
       <!--────────────────HEADER───────────────-->
       <header>
-          <h1 class="jumbotron" >Strategy Games</h1>  
+          <h1>Strategy Games</h1>  
 
       </header> <!-- / banner -->
